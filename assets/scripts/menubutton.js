3ca8a6650876b37d1301ccec50b4bd4c58a51a70
DraculCodes <draculcodes@gmail.com>
2021-03-21 02:36:26 +0100

    Alpha0.8 IoT lab images included

---
+++ assets/scripts/menubutton.js
@@ -0,0 +1,10 @@
+
+function menuClick(){
+    if(jQuery("headeroptions").is("visible") || jQuery("headeroptions").is("hidden")){
+        console.log("Menu options hidden");
+    }else{
+        console.log("Nothing at all");
+    }
+
+
+}
