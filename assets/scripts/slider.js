3ee93db52cfccede03c33292b6c407afd19f2a51
DraculCodes <draculcodes@gmail.com>
2021-03-24 00:30:00 +0100

    Alpha 0.9 IoT FABLAB and Rob. Lab section included

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,22 +1,25 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex,'iotimage');
+showSlides(slideIndex,'fablabimage');
+showSlides(slideIndex,'roboticsimage');
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+
+function plusSlides(n,classname) {
+    showSlides(slideIndex += n, classname);
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function currentSlide(n,classname) {
+    showSlides(slideIndex = n, classname);
 }
 
-function showSlides(n) {
+function showSlides(n, classname) {
     var i;
-    var slides = document.getElementsByClassName("slideimagediv");
+    var slides = document.getElementsByClassName(classname);
+    console.log(slides)
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-
     slides[slideIndex-1].style.display = "block";
 }
