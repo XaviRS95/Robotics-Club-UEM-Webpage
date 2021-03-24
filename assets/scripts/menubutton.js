6488b89b74a237b182948aab24a39a5db4e69171
DraculCodes <draculcodes@gmail.com>
2021-03-24 03:27:58 +0100

    Alpha 0.12 Responsive menu added

--- assets/scripts/menubutton.js
+++ assets/scripts/menubutton.js
@@ -1,10 +1,8 @@
 
-function menuClick(){
-    if(jQuery("headeroptions").is("visible") || jQuery("headeroptions").is("hidden")){
-        console.log("Menu options hidden");
-    }else{
-        console.log("Nothing at all");
+function menuClick() {
+    if (jQuery("#nav-menu").css("display")=="flex") {
+        jQuery("#nav-menu").css("display","none")
+    } else {
+        jQuery("#nav-menu").css("display","flex")
     }
-
-
 }
