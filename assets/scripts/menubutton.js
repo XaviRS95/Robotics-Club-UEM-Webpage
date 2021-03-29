bf0defca187c979754189893423a9d1c58ea9a88
DraculCodes <draculcodes@gmail.com>
2021-03-29 05:08:44 +0200

    Alpha 0.19 Menu redesigned to use only CSS to open and close

--- assets/scripts/menubutton.js
+++
@@ -1,8 +0,0 @@
-
-function menuClick() {
-    if (jQuery("#nav-menu").css("display")=="flex") {
-        jQuery("#nav-menu").css("display","none")
-    } else {
-        jQuery("#nav-menu").css("display","flex")
-    }
-}
